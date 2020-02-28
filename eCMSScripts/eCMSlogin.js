0ae4a889269d3da1376b55b049cf93d52a856473
jbravof <44535010+jbravof@users.noreply.github.com>
2020-02-27 23:50:06 -0500

    Update eCMSlogin.js

--- eCMSScripts/eCMSlogin.js
+++ eCMSScripts/eCMSlogin.js
@@ -2,3 +2,8 @@ function login_01() {
     //  alert("login-eCMS");
     window.location.assign("http://s104e7fd.dragados-usa.local:30000/ecms/login.faces")
 }
+
+function login_02() {
+    //  alert("login-eCMS");
+    document.getElementById("username").value = "jbravof";
+}
